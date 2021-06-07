8fbf1301177bf8984f327cf6f04b2becfd868eb2
Han Verstraete <welteki@pm.me>
2021-06-07 12:57:16 +0200

    Temporarily remove multi system support

--- flake.nix
+++ flake.nix
@@ -15,33 +15,30 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, flake-compat, inlets-src }:
-    with flake-utils.lib;
-    eachSystem (defaultSystems ++ [ "aarch64-darwin" "armv7l-linux" ]) (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
-        defaultPackage = pkgs.buildGoModule
-          rec {
-            pname = "inlets";
-            version = "3.0.2";
-            commit = "7b18a394b74390133e511957d954b1ba3b7d01a2";
+    {
+      system = "x86_64-linux";
+      defaultPackage = nixpkgs.buildGoModule
+        rec {
+          pname = "inlets";
+          version = "3.0.2";
+          commit = "7b18a394b74390133e511957d954b1ba3b7d01a2";
 
-            src = "${inlets-src}";
+          src = "${inlets-src}";
 
-            vendorSha256 = null;
+          vendorSha256 = null;
 
-            subPackages = [ "." ];
+          subPackages = [ "." ];
 
-            CGO_ENABLED = 0;
-            buildFlagsArray = [
-              ''
-                -ldflags=
-                -s -w 
-                -X main.GitCommit=${commit}
-                -X main.Version=${version}
-              ''
-              "-a"
-            ];
-          };
-      }
-    );
+          CGO_ENABLED = 0;
+          buildFlagsArray = [
+            ''
+              -ldflags=
+              -s -w 
+              -X main.GitCommit=${commit}
+              -X main.Version=${version}
+            ''
+            "-a"
+          ];
+        };
+    };
 }
