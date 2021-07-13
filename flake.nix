0d4092edb6c8f7972437bf492db0806443347d44
Han Verstraete <welteki@pm.me>
2021-07-13 15:07:29 +0200

    Add inlets-pro package

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "Cloud Native Tunnel";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.follows = "nix/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, nix, utils, ... }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -21,20 +22,32 @@
     {
       overlay = final: prev:
         let
-          inherit (final) buildGoModule fetchFromGitHub;
+          inherit (final) fetchurl buildGoModule stdenv system;
+
+          inlets-pro = rec {
+            version = "0.8.9";
+            src = {
+              inherit version;
+
+              x86_64-linux = fetchurl {
+                url = "https://github.com/inlets/inlets-pro/releases/download/${version}/inlets-pro";
+                sha256 = "07521b2g7saavda1m2jkva631s80f14ik9h9dg755whjqyx343lj";
+              };
+
+              x86_64-darwin = fetchurl {
+                url = "https://github.com/inlets/inlets-pro/releases/download/${version}/inlets-pro-darwin";
+                sha256 = "1wawrmi74y43l6x6bmdsdyxav45r4miia3v070kis3jq20r8pc9m";
+              };
+            };
+          };
         in
         {
           inlets = buildGoModule rec {
             pname = "inlets";
-            version = "4.0.1-archived";
-            commit = "ae9a0f82a8914c12b338f5c310e3fcf9750e0309";
+            version = "4.0.1";
+            commit = "883e3c42be9c1f53d63c8cb47407644387966f33";
 
-            src = fetchFromGitHub {
-              owner = "inlets";
-              repo = "inlets-archived";
-              rev = "${commit}";
-              sha256 = "1vbp718zcrwavz95h55pa97pyimyg3rkna0lrfbgyblcr81zq32i";
-            };
+            src = "${inputs.inlets-src}";
 
             vendorSha256 = "0jqkfjpvfwhx9dn58liawsyyn01bydp970fifc79vx126g0fczm9";
 
@@ -51,16 +64,38 @@
               "-a"
             ];
           };
-        };
 
-      defaultPackage = forAllSystems (system: (import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      }).inlets);
+          inlets-pro = stdenv.mkDerivation rec {
+            pname = "inlets-pro";
+            version = "${inlets-pro.version}";
+
+            src = inlets-pro.src.${system};
+
+            dontUnpack = true;
+
+            installPhase = ''
+              install -m755 -D ${src} $out/bin/inlets-pro
+            '';
+          };
+        };
 
       nixosModules.inlets = {
         imports = [ ./inlets-module.nix ];
         nixpkgs.overlays = [ self.overlay ];
       };
-    };
+
+    } // utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay nix.overlay ];
+        };
+      in
+      rec {
+        packages = utils.lib.flattenTree {
+          inlets = pkgs.inlets;
+          inlets-pro = pkgs.inlets-pro;
+        };
+        defaultPackage = packages.inlets-pro;
+      });
 }
