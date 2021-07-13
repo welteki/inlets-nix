82754e55aeb64f3382f9f746736a79a70de741b0
Han Verstraete <welteki@pm.me>
2021-07-13 15:08:46 +0200

    Remove untested systems

--- flake.nix
+++ flake.nix
@@ -17,12 +17,7 @@
     let
       supportedSystems = [
         "x86_64-linux"
-        "i686-linux"
         "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-        "aarch64-darwin"
       ];
 
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
