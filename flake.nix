c45639c5d204bd898873812da15ca85619b34125
Han Verstraete <welteki@pm.me>
2022-08-28 10:51:33 +0200

    Remove inlets oss

--- flake.nix
+++ flake.nix
@@ -42,29 +42,6 @@
           };
         in
         {
-          inlets = buildGoModule rec {
-            pname = "inlets";
-            version = "4.0.1";
-            commit = "883e3c42be9c1f53d63c8cb47407644387966f33";
-
-            src = "${inputs.inlets-src}";
-
-            vendorSha256 = "0jqkfjpvfwhx9dn58liawsyyn01bydp970fifc79vx126g0fczm9";
-
-            subPackages = [ "." ];
-
-            CGO_ENABLED = 0;
-            buildFlagsArray = [
-              ''
-                -ldflags=
-                -s -w 
-                -X main.GitCommit=${commit}
-                -X main.Version=${version}
-              ''
-              "-a"
-            ];
-          };
-
           inlets-pro = stdenv.mkDerivation rec {
             pname = "inlets-pro";
             version = "${inlets-pro.version}";
@@ -79,11 +56,6 @@
           };
         };
 
-      nixosModules.inlets = {
-        imports = [ ./inlets-module.nix ];
-        nixpkgs.overlays = [ self.overlay ];
-      };
-
     } // utils.lib.eachSystem supportedSystems (system:
       let
         pkgs = import nixpkgs {
@@ -93,7 +65,6 @@
       in
       rec {
         packages = utils.lib.flattenTree {
-          inlets = pkgs.inlets;
           inlets-pro = pkgs.inlets-pro;
         };
         defaultPackage = packages.inlets-pro;
