143b3810909322b21ffad2cb886feb6a2575e88a
Han Verstraete <welteki@pm.me>
2024-12-05 00:34:49 +0100

    0.9.9 -> 0.9.39

--- flake.nix
+++ flake.nix
@@ -20,28 +20,28 @@
           inherit (final) fetchurl buildGoModule stdenv system;
 
           inlets-pro = rec {
-            version = "0.9.9";
+            version = "0.9.39";
             src = {
               inherit version;
 
               x86_64-linux = fetchurl {
                 url = "https://github.com/inlets/inlets-pro/releases/download/${version}/inlets-pro";
-                sha256 = "sha256-jZHynTXzt/7TcivhM4RRqH8RQHDoM349CjkUouuocBU=";
+                sha256 = "sha256-usOdUd52Tgm5fJjmuP6ZqncGHUBS5jC+B1wgZkx4x1I=";
               };
 
               x86_64-darwin = fetchurl {
                 url = "https://github.com/inlets/inlets-pro/releases/download/${version}/inlets-pro-darwin";
-                sha256 = "sha256-CD7kTRMfN1ZzzorR3o/ub6pKFElz6+jOEFGRzmmhMpM=";
+                sha256 = "sha256-Q1rVQ+FaeYmAY5Uu9FvLT0Qh3v0XIEUvVEWnps0n70k=";
               };
 
               aarch64-linux = fetchurl {
                 url = "https://github.com/inlets/inlets-pro/releases/download/${version}/inlets-pro-arm64";
-                sha256 = "sha256-endv45vc5J4GenxeunpN6jxW7IJCI/allESO+D3MOhU=";
+                sha256 = "sha256-gPI18BtRuHJY0WO2aVd5amkjg2WYigi66y2ATn5NfEk=";
               };
 
               aarch64-darwin = fetchurl {
                 url = "https://github.com/inlets/inlets-pro/releases/download/${version}/inlets-pro-darwin-arm64";
-                sha256 = "sha256-DFZgulu+X8TfrBnTh0v8+knY2A3IAEnGd0yrSXCxLA4=";
+                sha256 = "sha256-QKbBkIK4Y3O8M4IKsIkccBe2tqSXsmZ8C8pG5+upZmM=";
               };
             };
           };
